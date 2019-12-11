d7e4e884c6abbece19dfd6130db368746662ca71
Pascal Riquier <riquipa@riquipa.local>
2019-12-11 13:19:45 +0100

    VOORTOETS-3016 initiele commit

---
+++ vl-proza-message.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlProzaMessage
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-proza-message/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-proza-message/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-proza-message.html|Demo}
+ * 
+ */
+export class VlProzaMessage extends VlElement(HTMLElement) {}
+
+define('vl-proza-message', VlProzaMessage);
---
+++ y/vl-blueprint.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlBlueprint
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-proza-message/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-proza-message/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-proza-message.html|Demo}
+ * 
+ */
+export class VlBlueprint extends VlElement(HTMLElement) {}
+
+define('vl-proza-message', VlBlueprint);
