efc6d8b0b931be571d64d7a4540510eea8d402ee
Tom Coemans <tom.coemans@gmail.com>
2020-02-07 12:58:30 +0100

    export classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = { 
-    VlProzaMessage: require('../vl-proza-message.src.js').VlProzaMessage
+    VlProzaMessage: require('../vl-proza-message.src.js').VlProzaMessage,
+    VlProzaMessagePreloader: require('../vl-proza-message.src.js').VlProzaMessagePreloader,
+    ProzaRestClient: require('../vl-proza-message.src.js').ProzaRestClient
 }
