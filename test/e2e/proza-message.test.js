ee441ca3fb38b2f4cd7cd39c0a324dd2f865d1bd
Pieter Beckers <pieter.beckers@vlaanderen.be>
2020-03-09 10:53:36 +0100

    UIG-378: beforeeach met message en in aftereach opkuisen

--- test/e2e/proza-message.test.js
+++ test/e2e/proza-message.test.js
@@ -3,6 +3,11 @@ const VlProzaMessagePage = require('./pages/vl-proza-message.page');
 
 describe('vl-proza-message', async () => {
     const vlProzaMessagePage = new VlProzaMessagePage(driver);
+    let message;
+
+    beforeEach(() => {
+        message = undefined;
+    });
 
     before(() => {
         return vlProzaMessagePage.load();
@@ -24,12 +29,11 @@ describe('vl-proza-message', async () => {
     });
 
     it('als gebruiker kan ik een tekst verwijderen', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await message.edit();
         await message.clear();
         await message.confirm();
         await assert.eventually.equal(message.getText(), '');
-        await resetMessage(message);
     });
 
     it('als gebruiker kan ik op de escape-toets drukken om mijn wijzigingen te annuleren', async () => {
@@ -42,63 +46,57 @@ describe('vl-proza-message', async () => {
     });
 
     it('als de gebruiker kan ik op de enter-toets drukken om mijn wijzigingen te bewaren', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await message.edit();
         await message.type('decibel');
         await message.confirm();
         await assert.eventually.equal(message.getText(), 'decibel');
-        await resetMessage(message);
     });
 
     it('als gebruiker kan ik enter+shift invoeren om een line-break toe te voegen', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await message.edit();
         await message.type('line');
         await message.shiftEnter();
         await message.append('break');
         await message.confirm();
         await assert.eventually.include(message.getText(), '\n');
-        await resetMessage(message);
     });
 
     it('als gebruiker kan ik de wysiwyg activeren door alle tekst te selecteren', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await message.edit();
         await message.selectAllText();
         await vlProzaMessagePage.waitUntilWysiwygOfMessageFirstDemoIsPresent();
         await assert.eventually.isTrue(message.isWysiwygPresent());
         await message.confirm();
-        await resetMessage(message);
     });
 
     it('als gebruiker kan ik bold stijl toevoegen door in de wysiwyg de stijl knop te gebruiken', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await enableWysiwyg(message);
         await assert.eventually.isFalse(message.hasBoldStyle());
         await vlProzaMessagePage.clickWysiwygBoldButton();
         await message.confirm();
         await assert.eventually.isTrue(message.hasBoldStyle());
-        await resetMessage(message);
     });
 
     it('als gebruiker kan ik italic stijl toevoegen door in de wysiwyg de stijl knop te gebruiken', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await enableWysiwyg(message);
         await assert.eventually.isFalse(message.hasItalicStyle());
         await vlProzaMessagePage.clickWysiwygItalicButton();
         await message.confirm();
         await assert.eventually.isTrue(message.hasItalicStyle());
-        await resetMessage(message);
     });
 
     it('als gebruiker kan ik underline stijl toevoegen door in de wysiwyg de stijl knop te gebruiken', async () => {
-        const message = await vlProzaMessagePage.getMessageFirstDemo();
+        message = await vlProzaMessagePage.getMessageFirstDemo();
         await enableWysiwyg(message);
         await assert.eventually.isFalse(message.hasUnderlineStyle());
         await vlProzaMessagePage.clickWysiwygUnderlineButton();
         await message.confirm();
         await assert.eventually.isTrue(message.hasUnderlineStyle());
-        await resetMessage(message);
     });
 
     async function enableWysiwyg(message) {
@@ -131,9 +129,11 @@ describe('vl-proza-message', async () => {
         await assert.eventually.equal(alert.getTitle(), 'Technische storing');
     });
 
-    async function resetMessage(message) {
-        await message.edit();
-        await message.type('foobar');
-        await message.confirm();
-    }
+    afterEach(async () => {
+        if (message) {
+            await message.edit();
+            await message.type('foobar');
+            await message.confirm();
+        }
+    });
 });
