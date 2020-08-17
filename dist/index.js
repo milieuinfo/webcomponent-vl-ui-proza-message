f96949aef1acfb6587065b1811b0f4667ceffccf
Tom Coemans <tom.coemans@gmail.com>
2020-08-17 15:43:16 +0200

    dist cache

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  VlProzaMessage: require('../dist/vl-proza-message.src.js').VlProzaMessage,
-  VlProzaMessagePreloader: require('../dist/vl-proza-message.src.js').VlProzaMessagePreloader,
-  ProzaRestClient: require('../dist/vl-proza-message.src.js').ProzaRestClient,
-};
